3b315e1073b2cc19a3b8a4f119c6b3d4c4bb4392
Lucas Rocha <ld.rocha@hotmail.com>
2021-05-05 12:44:09 -0700

    add fonts create chakra default custom theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      500: '#FFBA08',
+    },
+    yellowWithOpacity: {
+      500: 'rgba(255, 186, 8, 0.5)'
+    },
+    gray: {
+      500: '#999999',
+      800: '#47585B',
+    },
+    grayWithOpacity: {
+      500: 'rgba(153, 153, 153, 0.5)'
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'white',
+        color: 'gray.800'
+      }
+    }
+  }
+})
